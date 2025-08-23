287e67856827ffd56d2461aee862f637c38cb100
rishinduyohan <rishinduyohan@gmail.com>
2025-08-24 00:04:57 +0530

    Add video capture function

--- face_detector.py
+++ face_detector.py
@@ -1,3 +1,4 @@
 #pip install opencv-python
 import cv2
-a=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+a = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+b = cv2.VideoCapture(0)
