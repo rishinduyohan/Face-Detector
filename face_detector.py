b68aaa05af53ef6e6472c2c189b38730cbe5f44e
rishinduyohan <rishinduyohan@gmail.com>
2025-08-24 00:15:42 +0530

    Create a function to detect face

--- face_detector.py
+++ face_detector.py
@@ -2,3 +2,11 @@
 import cv2
 a = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 b = cv2.VideoCapture(0)
+
+while True:
+    c_rec,d_img = b.read()
+    e = cv2.cvtColor(d_img, cv2.COLOR_BGR2GRAY)
+    f = a.detectMultiScale(e, 1.3,6)
+
+    for (x1,y1,w1,h1) in f:
+        cv2.rectangle(d_image, (x1,y1), (x1+w1, y1+h1), (255,0,0), 2)
