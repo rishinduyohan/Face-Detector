1f2690b87fae928b39768406c47a13f2d42ec7f7
rishinduyohan <rishinduyohan@gmail.com>
2025-08-24 00:43:25 +0530

    Change variable names to find easily

--- face_detector.py
+++ face_detector.py
@@ -1,27 +1,27 @@
 import cv2
 
 # Load Haar Cascade
-a = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
 # Start webcam
-b = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(0)
 
 #for FPS calculate
-prev_time =0;
+prev_time = 0
 
 while True:
-    c_rec, d_img = b.read()
+    c_rec, d_img = capture.read()
     if not c_rec:
         break  # exit if camera not working
 
     # Convert to grayscale
-    e = cv2.cvtColor(d_img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(d_img, cv2.COLOR_BGR2GRAY)
 
     # Detect faces
-    f = a.detectMultiScale(e, 1.3, 6)
+    face = face_cascade.detectMultiScale(gray, 1.3, 6)
 
     # Draw rectangles
-    for (x1, y1, w1, h1) in f:
+    for (x1, y1, w1, h1) in face:
         cv2.rectangle(d_img, (x1, y1), (x1+w1, y1+h1), (0, 255, 0), 2)
 
     # Show image
